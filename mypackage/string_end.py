113cf4986e14777e879b2cb4201fbf66eaf4d9aa
Tyler Gorecki <ttg6nx@virginia.edu>
2024-07-12 13:13:04 -0400

    test

--- mypackage/string_end.py
+++ mypackage/string_end.py
@@ -1,8 +1,9 @@
 def ret_last_n(word:str, n:int):
     '''returns last n letters of a string'''
-    if len(word) <= n: return "length of word must be greater than n"
+    #if len(word) <= n: return "length of word must be greater than n"
 
-    return word[-n:]
+    #return word[-n:]
+    print(x)
 
 def foo():
     print(x)
